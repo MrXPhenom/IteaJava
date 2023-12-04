e1da9b4e5ecccf21a78638933f2462313f40de32
MrXPhenom <oleksandr.kalyushko92@gmail.com>
2023-12-04 06:23:35 +0200

    commit 1

--- src/HomeWork/HomeWork6/ArrayOfNumbers.java
+++ src/HomeWork/HomeWork6/ArrayOfNumbers.java
@@ -1,15 +1,22 @@
 package HomeWork.HomeWork6;
 
 public class ArrayOfNumbers {
-    public static void main(String[] args) {
-        int minimalDifference = 20;
-        int[] array = {5, 11, 7, 20};
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                int min = Math.abs(array[i] - array[j]);
-                minimalDifference = Math.min(min, minimalDifference);
+
+    public static int findMinDifference(int[] numbers) {
+        int minDifference = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                int difference = Math.abs(numbers[i] - numbers[j]);
+                if (difference < minDifference) {
+                    minDifference = difference;
+                }
             }
         }
-        System.out.println("Minimal difference is " + minimalDifference);
+        return minDifference;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 11, 7, 20};
+        System.out.println("Minimal difference is " + findMinDifference(array));
     }
 }
