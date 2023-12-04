ed547d8fa43efb78a05b44dabf300da0ae345aed
MrXPhenom <oleksandr.kalyushko92@gmail.com>
2023-12-04 11:21:00 +0200

    commit 1

--- src/HomeWork/HomeWork5/Part2/Cat.java
+++ src/HomeWork/HomeWork5/Part2/Cat.java
@@ -1,6 +1,6 @@
 package HomeWork.HomeWork5.Part2;
 
-public class Cat extends Pet{
+public class Cat extends Pet {
     public Cat(String name) {
         super(name);
         System.out.println("I am a cat, and my name is " + name);
--- src/HomeWork/HomeWork5/Part2/Dog.java
+++ src/HomeWork/HomeWork5/Part2/Dog.java
@@ -1,6 +1,6 @@
 package HomeWork.HomeWork5.Part2;
 
-public class Dog extends Pet{
+public class Dog extends Pet {
     public Dog(String name) {
         super(name);
         System.out.println("I am a dog, and my name is " + name);
